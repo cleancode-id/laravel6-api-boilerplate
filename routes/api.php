44bee3e7d703bf94a7b7673fc3abc4ca01056b21
Yoga Hanggara <yohang88@gmail.com>
2019-09-07 01:18:26 +0700

    setup base routing
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,13 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/', function () {
+    return [
+        'app' => 'Laravel',
+        'version' => '1.0.0',
+    ];
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
