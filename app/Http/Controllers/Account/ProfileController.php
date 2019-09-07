330dc641bda55a0faf289d0f622bc71abf7304f2
Yoga Hanggara <yohang88@gmail.com>
2019-09-07 12:52:29 +0700

    update profile and password
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

---
+++ app/Http/Controllers/Account/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Account;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\User as UserResource;
+
+class ProfileController extends Controller
+{
+    /**
+     * Update the user's profile information.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $user = $request->user();
+
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,'.$user->id,
+        ]);
+
+        $user->update($request->only('name', 'email'));
+
+        return new UserResource($user);
+    }
+}
