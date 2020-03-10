d46a8e961b1d01e7c58825121a15ee149eeb6e4b
Putra Prima Arhandi <putraprima@gmail.com>
2020-03-10 21:01:17 +0700

    Cleaning controller and fixing middleware

--- app/Http/Controllers/Account/ProfileController.php
+++ app/Http/Controllers/Account/ProfileController.php
@@ -18,12 +18,6 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request)
     {
         $user = $request->user();
-
-//        $this->validate($request, [
-//            'name' => 'required',
-//            'email' => 'required|email|unique:users,email,'.$user->id,
-//        ]);
-
         $user->update($request->only('name', 'email'));
 
         return new UserResource($user);
--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use JWTAuth;
-use Exception;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
 class JwtMiddleware extends BaseMiddleware
